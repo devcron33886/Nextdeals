018e788354557b75ca4d0a8a6c2398b6d2c0962b
MBABAZI Jacques <mbabazijacques@gmail.com>
2022-07-29 11:22:27 +0200

    fixed home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -311,7 +311,7 @@
                                                 <div class="click">
                                                     @if ($latest->photo)
                                                         <div><a href="{{ route('product-show', $latest->slug) }}"><img
-                                                                    src="{{ $product->photo->getUrl('preview') }}"
+                                                                    src="{{ $latest->photo->getUrl('preview') }}"
                                                                     class="img-fluid mx-auto" alt="" /></a>
                                                         </div>
                                                     @endif
